f2928ef9ca381d834fa73426725766be06b2a1ae
gabriella <gabriella.mosca@gmail.com>
2020-06-17 11:17:31 +0200

    Added robustness analysis varying stiffness and growth signal by 10%

---
+++ robustnessAnalysis/0d22GrowthSigna/MorphoDynamX.py
@@ -0,0 +1,11 @@
+######## New MorphoDynamX session v2.0 r2-689: 2020-06-15 12:54:39
+Process.Tools__System__Save_View('/home/gabriella/IMAGO_project/FEM_2DOvuleGrowthModel/case1/FemMembranes.mdxv')
+Process.Model__CCF__01_FEM_Membranes('Model/CCF/02 FEM Solver', 'Model/CCF/14 Set Trichome Growth', 'Model/CCF/13 Growth', 'Model/CCF/14 Subdivide', '11, 22, 24, 26, 32, 37, 48, 49', 'Mesh/System/Save', 'OvuleFEMGrowth_case1_0d22Growth')
+Process.Mesh__System__Load('/home/gabriella/IMAGO_project/FEM_2DOvuleGrowthModel/case1/robustnessAnalysis/0d22GrowthSigna/OvuleFEMGrowth_case1_0d22Growth-0049.mdxm', 'no', 'no', '0')
+Process.Mesh__Attributes__Manage_Attributes('0')
+Process.Model__CCF__05_Stress-Strain('Stress', 'Strain', 'Stress', 'Strain', '1.0', 'Linear Triangle', 'Triangle Element', 'TransIso Material')
+Process.Model__CCF__18_Visualize_Directions('Material Direction', 'Growth Direction', '20', 'Linear Triangle', 'Triangle Element')
+Process.Mesh__Attributes__Manage_Attributes('0')
+Process.Model__CCF__05_Stress-Strain('Stress', 'Strain', 'Stress', 'Strain', '1.0', 'Linear Triangle', 'Triangle Element', 'TransIso Material')
+Process.Mesh__System__Save('/home/gabriella/IMAGO_project/FEM_2DOvuleGrowthModel/case1/robustnessAnalysis/0d22GrowthSigna/OvuleFEMGrowth_case1_0d22Growth-0049.mdxm', 'no', '0')
+Process.Tools__System__Load_View('/home/gabriella/IMAGO_project/FEM_2Dsimulations/FEM_2DSimulations/3linesEpidermis/FemMembranes.mdxv')
---
+++ robustnessAnalysis/MorphoDynamX.py
@@ -0,0 +1,18 @@
+######## New MorphoDynamX session v2.0 r2-689: 2020-06-15 17:16:41
+Process.Tools__System__Save_View('/home/gabriella/IMAGO_project/FEM_2Dsimulations/FEM_2DSimulations/3linesEpidermis/1+2+3+4+5+6/FemMembranesComparison.mdxv')
+######## New MorphoDynamX session v2.0 r2-689: 2020-06-16 13:01:53
+Process.Tools__System__Load_View('/home/gabriella/IMAGO_project/FEM_2DOvuleGrowthModel/case1/robustnessAnalysis/FemMembranesComparison.mdxv')
+Process.Mesh__System__Load('/home/gabriella/IMAGO_project/FEM_2DOvuleGrowthModel/case1/robustnessAnalysis/0d22GrowthSigna/OvuleFEMGrowth_case1_0d22Growth-0049.mdxm', 'no', 'no', '0')
+Process.Tools__System__Load_View('/home/gabriella/IMAGO_project/FEM_2DOvuleGrowthModel/case1/robustnessAnalysis/FemMembranesComparison.mdxv')
+######## New MorphoDynamX session v2.0 r2-689: 2020-06-16 16:15:02
+Process.Tools__System__Load_View('/home/gabriella/IMAGO_project/FEM_2DOvuleGrowthModel/case1/robustnessAnalysis/FemMembranesComparison.mdxv')
+Process.Mesh__System__Load('/home/gabriella/IMAGO_project/FEM_2DOvuleGrowthModel/case1/robustnessAnalysis/0d22GrowthSigna/OvuleFEMGrowth_case1_0d22Growth-0049.mdxm', 'no', 'no', '0')
+Process.Mesh__System__Load('/home/gabriella/IMAGO_project/FEM_2Dsimulations/FEM_2DSimulations/3linesEpidermis/1+2+3+4+5+6+7/OvuleFEMGrowth_1234567_refined5e-9-0049.mdxm', 'no', 'no', '1')
+Process.Tools__System__Load_View('/home/gabriella/IMAGO_project/FEM_2DOvuleGrowthModel/case1/robustnessAnalysis/FemMembranesComparison.mdxv')
+######## New MorphoDynamX session v2.0 r2-689: 2020-06-16 20:11:20
+Process.Tools__System__Load_View('/home/gabriella/IMAGO_project/FEM_2DOvuleGrowthModel/case1/robustnessAnalysis/FemMembranesComparison.mdxv')
+Process.Mesh__System__Load('/home/gabriella/IMAGO_project/FEM_2DOvuleGrowthModel/case1/robustnessAnalysis/0d22GrowthSigna/OvuleFEMGrowth_case1_0d22Growth-0049.mdxm', 'no', 'no', '0')
+Process.Tools__System__Load_View('/home/gabriella/IMAGO_project/FEM_2DOvuleGrowthModel/case1/robustnessAnalysis/FemMembranesComparison.mdxv')
+######## New MorphoDynamX session v2.0 r2-689: 2020-06-16 22:52:29
+Process.Tools__System__Load_View('/home/gabriella/IMAGO_project/FEM_2DOvuleGrowthModel/case1/robustnessAnalysis/FemMembranesComparison.mdxv')
+Process.Mesh__System__Load('/home/gabriella/IMAGO_project/FEM_2DOvuleGrowthModel/case1/robustnessAnalysis/0d22GrowthSigna/OvuleFEMGrowth_case1_0d22Growth-0049.mdxm', 'no', 'no', '0')
